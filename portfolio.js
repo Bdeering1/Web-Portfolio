075cfc8c37fc04e91f7da29b06036c430b2f23cb
Bryn Deering <bdeering@uoguelph.ca>
2021-01-31 01:09:19 -0500

    project section improvements, background more responsive

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,6 @@
 function parallax() {
     let s = document.getElementById("img-container");
-    if (window.innerWidth > 1300) {
-        var yPos = window.pageYOffset / (window.innerHeight / 38);
-    } else if (window.innerWidth > 850) {
-        var yPos = window.pageYOffset / (window.innerHeight / 56);
-    } else {
-        var yPos = window.pageYOffset / (window.innerHeight / 64);
-    }
+    var yPos = -window.pageYOffset / 20;
     s.style.top = yPos + "%";
 }
 
