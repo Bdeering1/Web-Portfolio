88ae6f5d6abb114433095d99fc45ceb92cff8a0c
Bryn Deering <bdeering@uoguelph.ca>
2021-01-31 01:31:16 -0500

    fixed scrolling on mobile

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,9 @@
 function parallax() {
-    let s = document.getElementById("img-container");
-    var yPos = -window.pageYOffset / 20;
-    s.style.top = yPos + "%";
+    if (window.innerHeight <= 1280) {
+        let s = document.getElementById("img-container");
+        var yPos = -window.pageYOffset / 20;
+        s.style.top = yPos + "%";
+    }
 }
 
 window.addEventListener("scroll", function () {
