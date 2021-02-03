5f4d1eddc7c1a14aa48a853d3eb7a2000b24f18c
Bryn Deering <bdeering@uoguelph.ca>
2021-02-03 01:21:36 -0500

    UI update

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,15 @@
 function parallax() {
     if (window.innerHeight <= 1280) {
         let s = document.getElementById("img-container");
-        var yPos = -window.pageYOffset / 20;
+        var yPos = -window.pageYOffset / 30;
         s.style.top = yPos + "%";
     }
 }
 
 window.addEventListener("scroll", function () {
     parallax();
+});
+
+window.addEventListener("onload", function () {
+
 });
