54341200e7d7fb2c1d20c5b96de556ea02f2219d
Bryn Deering <bdeering@uoguelph.ca>
2021-03-14 01:35:28 -0500

    UI changes

--- portfolio.js
+++ portfolio.js
@@ -8,13 +8,29 @@ function parallax() {
 
 window.addEventListener("scroll", parallax);
 
-/* window.onload = () => {
-    var lastScrollTop = window.pageYOffset;
-    let home = document.getElementById("home");
-    home.addEventListener("wheel", (event) => {
-        if (event.wheelDelta < 0 || event.deltaY > 0) {
-            document.getElementById("projects").scrollIntoView(true);
-        } else {
-        }
-    }, false);
-}; */
+window.onload = () => {
+    projects = document.getElementById("projects");
+    scrolled = false;
+}
+
+window.onscroll = function(e) {
+    if (this.oldScroll <= this.scrollY && scrolled == false) {
+        this.scrolled = true;
+        window.scroll({
+            top: findPos(projects),
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+    this.oldScroll = this.scrollY;
+}
+
+function findPos(obj) {
+    var curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+    return [curtop];
+    }
+}
