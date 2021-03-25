2bc7bd0b73feeaea63195017c4b3a817b3d6bd16
Bryn Deering <bdeering@uoguelph.ca>
2021-03-24 20:46:05 -0400

    First draft of about section, visual revamp

--- portfolio.js
+++ portfolio.js
@@ -6,28 +6,32 @@ const projects = [
         link: "https://bdeering1.github.io/Visual-Sorting-App",
         image: "resources/Visual Sorting.png",
         title: "Visual Sorting App",
-        desc: "Sorting algorithm learning tool (React and Redux)",
+        desc: "Sorting algorithm learning tool",
+        tech: "React & Redux",
         more: "• Fully responsive and cross browser compatible\n• Modular and scalable design"
     },
     {
         link: "https://bdeering1.github.io/To-Do-List/",
         image: "resources/To Do List.png",
         title: "To Do List",
-        desc: "Personal organization tool (Javascript and SASS)",
+        desc: "Personal organization tool",
+        tech: "Javascript & SASS",
         more: "• event listeners, DOM manipulation, and browser storage\n• css custom properties, pseudo elements, layouts"
     },
     {
         link: "https://codepen.io/bdeering1/full/QWKxemq",
         image: "resources/Calculator.png",
         title: "Calculator",
-        desc: "Virtual calculator app (React and SASS)",
+        desc: "Virtual calculator app",
+        tech: "React & SASS",
         more: "• responsize design\n• component functions and state manipulation"
     },
     {
         link: "https://codepen.io/bdeering1/full/OJRZWEy",
         image: "resources/Drum Machine.png",
         title: "Drum Machine",
-        desc: "Virtual drum machine (React and Bootstrap 4)",
+        desc: "Virtual drum machine",
+        tech: "React & Bootstrap 4",
         more: "• lifecycle methods, ES6 functionality\n• bootstrap integration"
     }
 ]
@@ -37,12 +41,14 @@ window.onload = () => {
         const projectTile = document.importNode(projectTemplate.content, true);
         const anchor = projectTile.querySelector('a');
         anchor.href = proj.link;
-        const title = projectTile.querySelector('.project-title');
+        const title = projectTile.querySelector('.project-header');
         title.innerHTML = proj.title;
-        const desc = projectTile.querySelector('.project-desc');
+        const desc = projectTile.querySelector('.project-desc i');
         desc.innerHTML = proj.desc;
         const more = projectTile.querySelector('.project-features');
         more.innerHTML = proj.more;
+        const tech = projectTile.querySelector('.project-tech');
+        tech.innerHTML = proj.tech;
         const image = projectTile.querySelector('.project-img');
         image.src = proj.image;
         image.alt = proj.title + " project image";
